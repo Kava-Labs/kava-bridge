f25e565239ba545bafd6106eefcba6ff4702c440
drklee3 <derrick@dlee.dev>
2022-03-01 10:36:39 -0800

    Add erc20 func

---
+++ x/bridge/keeper/erc20.go
@@ -0,0 +1,10 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/kava-labs/kava-bridge/x/bridge/types"
+)
+
+func (k Keeper) DeployERC20Contract(ctx sdk.Context, token types.EnabledERC20Token) {
+
+}
