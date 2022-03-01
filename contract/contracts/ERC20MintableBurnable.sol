42a9be93425725394019de9c03a396303c3bff50
drklee3 <derrick@dlee.dev>
2022-02-28 16:51:44 -0800

    Add Mintable ERC20

---
+++ contract/contracts/ERC20MintableBurnable.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title A contract for an mintable and burnable ERC20 that is handled by the bridge Cosmos SDK module account
+/// @author Kava Labs, LLC
+/// @custom:security-contact security@kava.io
+contract ERC20MintableBurnable is ERC20, ERC20Burnable, Ownable {
+    uint8 immutable private _decimals;
+
+    /// @notice Registers the ERC20 token with mint and burn permissions for the
+    ///         contract owner, by default the account that deploys this contract.
+    /// @param name The name of the ERC20 token.
+    /// @param symbol The symbol of the ERC20 token.
+    /// @param decimals_ The number of decimals of the ERC20 token.
+    constructor(string memory name, string memory symbol, uint8 decimals_) ERC20(name, symbol) {
+        _decimals = decimals_;
+    }
+
+    /// @notice Creates more supply for a given account and amount by contract owner.
+    /// @param to The account to send the minted supply to.
+    /// @param amount The amount of the token to mint.
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+}
