42e179485a12f56e06f40b84a3bb6cee7f8617d4
drklee3 <derrick@dlee.dev>
2022-03-02 11:14:23 -0800

    Fix erc20 deploy test setup
    Was using the wrong private key to set as validator concensus pubkey

--- x/bridge/keeper/erc20_test.go
+++ x/bridge/keeper/erc20_test.go
@@ -2,7 +2,6 @@ package keeper_test
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 	"time"
 
@@ -14,7 +13,6 @@ import (
 	"github.com/tendermint/tendermint/crypto/tmhash"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmversion "github.com/tendermint/tendermint/proto/tendermint/version"
-	tmtime "github.com/tendermint/tendermint/types/time"
 	"github.com/tendermint/tendermint/version"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -26,35 +24,78 @@ import (
 	evmtypes "github.com/tharsis/ethermint/x/evm/types"
 
 	"github.com/kava-labs/kava-bridge/app"
-	"github.com/kava-labs/kava-bridge/x/bridge/keeper"
 	"github.com/kava-labs/kava-bridge/x/bridge/types"
 )
 
 type ERC20TestSuite struct {
 	suite.Suite
 
-	App          app.TestApp
-	bridgeKeeper keeper.Keeper
-	Ctx          sdk.Context
+	ctx            sdk.Context
+	app            app.TestApp
+	address        common.Address
+	consAddress    sdk.ConsAddress
+	relayerAddress sdk.AccAddress
 }
 
 func (suite *ERC20TestSuite) SetupTest() {
-	suite.App = app.NewTestApp()
+	suite.app = app.NewTestApp()
 
 	// consensus key
-	priv, err := ethsecp256k1.GenerateKey()
+	consPriv, err := ethsecp256k1.GenerateKey()
 	suite.Require().NoError(err)
-	consAddress := sdk.ConsAddress(priv.PubKey().Address())
+	suite.consAddress = sdk.ConsAddress(consPriv.PubKey().Address())
 
-	suite.bridgeKeeper = suite.App.GetBridgeKeeper()
-	// First is validator, second is bridge relayer
-	keys, addrs := app.GeneratePrivKeyAddressPairs(3)
+	// relayer key
+	relayerPriv, err := ethsecp256k1.GenerateKey()
+	suite.Require().NoError(err)
+	suite.relayerAddress = sdk.AccAddress(relayerPriv.PubKey().Address())
 
-	suite.Ctx = suite.App.NewContext(true, tmproto.Header{
+	// Genesis states
+	evmGs := evmtypes.NewGenesisState(
+		evmtypes.NewParams("ukava", true, true, evmtypes.DefaultChainConfig()),
+		nil,
+	)
+
+	bridgeGs := types.NewGenesisState(types.NewParams(
+		types.EnabledERC20Tokens{
+			types.NewEnabledERC20Token(
+				"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+				"Wrapped ETH",
+				"WETH",
+				18,
+			),
+		},
+		suite.relayerAddress,
+	))
+
+	genesisState := app.NewDefaultGenesisState()
+	genesisState[types.ModuleName] = suite.app.AppCodec().MustMarshalJSON(&bridgeGs)
+	genesisState[evmtypes.ModuleName] = suite.app.AppCodec().MustMarshalJSON(evmGs)
+
+	// Initialize the chain
+	stateBytes, err := json.Marshal(genesisState)
+	suite.Require().NoError(err)
+	suite.app.InitChain(
+		abci.RequestInitChain{
+			Time:          time.Now().UTC(),
+			Validators:    []abci.ValidatorUpdate{},
+			AppStateBytes: stateBytes,
+			ChainId:       "kavatest_8888-1",
+			// Set consensus params, which is needed by x/feemarket
+			ConsensusParams: &abci.ConsensusParams{
+				Block: &abci.BlockParams{
+					MaxBytes: 200000,
+					MaxGas:   20000000,
+				},
+			},
+		},
+	)
+
+	suite.ctx = suite.app.NewContext(false, tmproto.Header{
 		Height:          1,
-		ChainID:         "kavatest_1-1",
-		Time:            tmtime.Now(),
-		ProposerAddress: consAddress.Bytes(),
+		ChainID:         "kavatest_8888-1",
+		Time:            time.Now().UTC(),
+		ProposerAddress: suite.consAddress.Bytes(),
 		Version: tmversion.Consensus{
 			Block: version.BlockProtocol,
 		},
@@ -78,77 +119,20 @@ func (suite *ERC20TestSuite) SetupTest() {
 	// https://github.com/tharsis/ethermint/blob/f21592ebfe74da7590eb42ed926dae970b2a9a3f/x/evm/keeper/state_transition.go#L487
 	// evmkeeper.EVMConfig() will return error "failed to load evm config" if not set
 	acc := &etherminttypes.EthAccount{
-		BaseAccount: authtypes.NewBaseAccount(sdk.AccAddress(addrs[0].Bytes()), nil, 0, 0),
+		BaseAccount: authtypes.NewBaseAccount(sdk.AccAddress(suite.address.Bytes()), nil, 0, 0),
 		CodeHash:    common.BytesToHash(crypto.Keccak256(nil)).String(),
 	}
 
-	suite.App.AccountKeeper.SetAccount(suite.Ctx, acc)
+	suite.app.AccountKeeper.SetAccount(suite.ctx, acc)
 
-	valAddr := sdk.ValAddress(addrs[0].Bytes())
-	validator, err := stakingtypes.NewValidator(valAddr, keys[0].PubKey(), stakingtypes.Description{})
+	valAddr := sdk.ValAddress(suite.address.Bytes())
+	validator, err := stakingtypes.NewValidator(valAddr, consPriv.PubKey(), stakingtypes.Description{})
 	suite.Require().NoError(err)
-	err = suite.App.StakingKeeper.SetValidatorByConsAddr(suite.Ctx, validator)
+
+	err = suite.app.StakingKeeper.SetValidatorByConsAddr(suite.ctx, validator)
 	suite.Require().NoError(err)
-	suite.App.StakingKeeper.SetValidator(suite.Ctx, validator)
 
-	bridgeGs := types.NewGenesisState(types.NewParams(
-		types.EnabledERC20Tokens{
-			types.NewEnabledERC20Token(
-				"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-				"Wrapped ETH",
-				"WETH",
-				18,
-			),
-		},
-		addrs[1],
-	))
-
-	evmGs := evmtypes.NewGenesisState(
-		evmtypes.NewParams("ukava", true, true, evmtypes.DefaultChainConfig()),
-		nil,
-	)
-
-	evmGsBytes := app.GenesisState{evmtypes.ModuleName: suite.App.AppCodec().MustMarshalJSON(evmGs)}
-
-	moduleGs := suite.App.AppCodec().MustMarshalJSON(&bridgeGs)
-	gs := app.GenesisState{types.ModuleName: moduleGs}
-
-	genesisStates := []app.GenesisState{evmGsBytes, gs}
-
-	genesisState := app.NewDefaultGenesisState()
-	for _, state := range genesisStates {
-		for k, v := range state {
-			genesisState[k] = v
-		}
-	}
-
-	// Initialize the chain
-	stateBytes, err := json.Marshal(genesisState)
-	if err != nil {
-		panic(err)
-	}
-	suite.App.InitChain(
-		abci.RequestInitChain{
-			Time:          time.Now().UTC(),
-			Validators:    []abci.ValidatorUpdate{},
-			AppStateBytes: stateBytes,
-			ChainId:       "kavatest_1-1",
-			// Set consensus params, which is needed by x/feemarket
-			ConsensusParams: &abci.ConsensusParams{
-				Block: &abci.BlockParams{
-					MaxBytes: 200000,
-					MaxGas:   20000000,
-				},
-			},
-		},
-	)
-
-	suite.Commit()
-
-	fmt.Printf("consAddr: %v\n", consAddress)
-	fmt.Printf("consAddr from block: %v\n", sdk.ConsAddress(suite.Ctx.BlockHeader().ProposerAddress))
-	validator, found := suite.App.StakingKeeper.GetValidatorByConsAddr(suite.Ctx, consAddress)
-	fmt.Printf("GetValidatorByConsAddr (found: %v): %v\n", found, validator)
+	suite.app.StakingKeeper.SetValidator(suite.ctx, validator)
 }
 
 func TestERC20TestSuite(t *testing.T) {
@@ -156,15 +140,15 @@ func TestERC20TestSuite(t *testing.T) {
 }
 
 func (suite *ERC20TestSuite) Commit() {
-	_ = suite.App.Commit()
-	header := suite.Ctx.BlockHeader()
+	_ = suite.app.Commit()
+	header := suite.ctx.BlockHeader()
 	header.Height += 1
-	suite.App.BeginBlock(abci.RequestBeginBlock{
+	suite.app.BeginBlock(abci.RequestBeginBlock{
 		Header: header,
 	})
 
 	// update ctx
-	suite.Ctx = suite.App.NewContext(false, header)
+	suite.ctx = suite.app.NewContext(false, header)
 }
 
 func (suite *ERC20TestSuite) TestDeployERC20() {
@@ -175,7 +159,7 @@ func (suite *ERC20TestSuite) TestDeployERC20() {
 		18,
 	)
 
-	addr, err := suite.bridgeKeeper.DeployMintableERC20Contract(suite.Ctx, token)
+	addr, err := suite.app.BridgeKeeper.DeployMintableERC20Contract(suite.ctx, token)
 	suite.Require().NoError(err)
-	suite.Require().Greater(0, len(addr))
+	suite.Require().Greater(len(addr), 0)
 }
