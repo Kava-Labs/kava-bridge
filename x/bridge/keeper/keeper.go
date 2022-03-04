0e3ce8f95b3bc108b2c0d8ee7df0594824ce5d55
drklee3 <derrick@dlee.dev>
2022-03-03 17:42:08 -0800

    Remove keeper methods

--- x/bridge/keeper/keeper.go
+++ x/bridge/keeper/keeper.go
@@ -2,10 +2,8 @@ package keeper
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/kava-labs/kava-bridge/x/bridge/types"
 )
@@ -41,37 +39,4 @@ func NewKeeper(
 		accountKeeper: ak,
 		evmKeeper:     ek,
 	}
-}
-
-// BridgedInternalEVMAddress puts the bridged address pair into the store.
-func (k Keeper) SetBridgedEVMAddress(
-	ctx sdk.Context,
-	externalAddress types.ExternalEVMAddress,
-	internalAddress types.InternalEVMAddress,
-) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.BridgedERC20KeyPrefix)
-	store.Set(types.GetBridgedERC20Key(externalAddress), internalAddress.Bytes())
-}
-
-// GetBridgedInternalEVMAddress gets the internal EVM address mapped to the
-// provided ExternalEVMAddress from the store.
-func (k Keeper) GetBridgedInternalEVMAddress(
-	ctx sdk.Context,
-	externalAddress types.ExternalEVMAddress,
-) (types.InternalEVMAddress, bool) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.BridgedERC20KeyPrefix)
-	bz := store.Get(types.GetBridgedERC20Key(externalAddress))
-	if bz == nil {
-		return types.InternalEVMAddress{}, false
-	}
-
-	return types.InternalEVMAddress{
-		Address: common.BytesToAddress(bz),
-	}, true
-}
-
-// DeleteBridgedEVMAddress removes an bridged address pair from the store.
-func (k Keeper) DeleteBridgedEVMAddress(ctx sdk.Context, externalAddress types.ExternalEVMAddress) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.BridgedERC20KeyPrefix)
-	store.Delete(types.GetBridgedERC20Key(externalAddress))
 }
