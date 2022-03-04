d4279740a7d82c40ccbf303bb267cfab8af36e31
denalimarsh <denalimarsh@gmail.com>
2022-03-04 15:38:50 +0100

    linter

--- contract/test/wkava_test.ts
+++ contract/test/wkava_test.ts
@@ -7,69 +7,67 @@ import {
 } from "../typechain-types";
 
 describe("WKAVA", function () {
-  let wkava: WKAVA;
-  let wkavaFactory: WKAVAFactory;
-  let addr1: Signer;
-  let addr2: Signer;
+ let wkava: WKAVA;
+ let wkavaFactory: WKAVAFactory;
+ let addr1: Signer;
 
   beforeEach(async function () {
     wkavaFactory = await ethers.getContractFactory("WKAVA");
     wkava = await wkavaFactory.deploy();
     addr1 = (await ethers.getSigners())[1];
-    addr2 = (await ethers.getSigners())[2];
   });
 
   describe("Initialization values", function () {
-        it("should have correct name", async function () {
-            expect(await wkava.name()).to.be.equal("Wrapped Kava");
-        });
-
-        it("should have correct symbol", async function () {
-            expect(await wkava.symbol()).to.be.equal("WKAVA");
-        });
-
-        it("should have correct number of decimals", async function () {
-            expect(await wkava.decimals()).to.be.equal(6);
-        });
+    it("should have correct name", async function () {
+      expect(await wkava.name()).to.be.equal("Wrapped Kava");
     });
+
+    it("should have correct symbol", async function () {
+      expect(await wkava.symbol()).to.be.equal("WKAVA");
+    });
+
+    it("should have correct number of decimals", async function () {
+      expect(await wkava.decimals()).to.be.equal(6);
+    });
+  });
 
   describe("deposit", function () {
     it("should allow users to deposit", async function () {
-        const userBalanceBefore = await wkava.balanceOf(await addr2.getAddress());
-        expect(userBalanceBefore).to.equal(0);
+      const userBalanceBefore = await wkava.balanceOf(await addr1.getAddress());
+      expect(userBalanceBefore).to.equal(0);
 
-        const depositAmt = 5000;
-        await wkava.connect(addr2).deposit({ value: depositAmt });
+      const depositAmt = 5000;
+      await wkava.connect(addr1).deposit({ value: depositAmt });
 
-        const userBalanceAfter = await wkava.balanceOf(await addr2.getAddress());
-        expect(userBalanceAfter).to.equal(depositAmt);
+      const userBalanceAfter = await wkava.balanceOf(await addr1.getAddress());
+      expect(userBalanceAfter).to.equal(depositAmt);
     });
 
     it("should accept ethereum sent directly to the contract", async function () {
-      const userBalanceBefore = await wkava.balanceOf(await addr2.getAddress());
+      const userBalanceBefore = await wkava.balanceOf(await addr1.getAddress());
       expect(userBalanceBefore).to.equal(0);
 
       const depositAmt = ethers.utils.parseEther("1.0");
-      await addr2.sendTransaction({
+      await addr1.sendTransaction({
         to: wkava.address,
         value: depositAmt, // Sends exactly 1.0 ether
       });
 
-      const userBalanceAfter = await wkava.balanceOf(await addr2.getAddress());
+      const userBalanceAfter = await wkava.balanceOf(await addr1.getAddress());
       expect(userBalanceAfter).to.equal(depositAmt);
-  });
+    });
 
     it("should emit Deposit event", async function () {
-        const depositAmt = 5000;
-        const tx: ContractTransaction = await wkava.connect(addr2).deposit({ value: depositAmt });
-        let receipt: ContractReceipt = await tx.wait();
+      const depositAmt = 5000;
+      const tx: ContractTransaction = await wkava.connect(addr1).deposit({ value: depositAmt });
+      let receipt: ContractReceipt = await tx.wait();
 
-        const event = receipt.events?.find((x: any) => {
-          return x.event === "Deposit";
-        });
+      const event = receipt.events?.find((x: any) => {
+        return x.event === "Deposit";
+      });
 
-        expect(event?.args?.src).to.equal(await addr2.getAddress());
-        expect(event?.args?.wad).to.equal(depositAmt);
+      expect(event?.args?.src).to.equal(await addr1.getAddress());
+      expect(event?.args?.wad).to.equal(depositAmt);
     });
   });
 
@@ -78,34 +76,34 @@ describe("WKAVA", function () {
     let depositAmt = 5000;
 
     beforeEach(async function () {
-      await wkava.connect(addr2).deposit({ value: depositAmt });
+      await wkava.connect(addr1).deposit({ value: depositAmt });
     });
 
     it("should allow users to withdraw", async function () {
-        const userBalanceBefore = await wkava.balanceOf(await addr2.getAddress());
-        expect(userBalanceBefore).to.equal(depositAmt);
+      const userBalanceBefore = await wkava.balanceOf(await addr1.getAddress());
+      expect(userBalanceBefore).to.equal(depositAmt);
 
-        const withdrawAmt = depositAmt/2;
-        await wkava.connect(addr2).withdraw(withdrawAmt);
+      const withdrawAmt = depositAmt/2;
+      await wkava.connect(addr1).withdraw(withdrawAmt);
 
-        const userBalanceAfter = await wkava.balanceOf(await addr2.getAddress());
-        expect(userBalanceAfter).to.equal(depositAmt-withdrawAmt);
+      const userBalanceAfter = await wkava.balanceOf(await addr1.getAddress());
+      expect(userBalanceAfter).to.equal(depositAmt-withdrawAmt);
 
-        await wkava.connect(addr2).withdraw(withdrawAmt);
-        const userBalanceFinal = await wkava.balanceOf(await addr2.getAddress());
-        expect(userBalanceFinal).to.equal(0);
+      await wkava.connect(addr1).withdraw(withdrawAmt);
+      const userBalanceFinal = await wkava.balanceOf(await addr1.getAddress());
+      expect(userBalanceFinal).to.equal(0);
     });
 
     it("should emit Withdrawal event", async function () {
-        const tx: ContractTransaction = await wkava.connect(addr2).withdraw(depositAmt);
-        let receipt: ContractReceipt = await tx.wait();
+      const tx: ContractTransaction = await wkava.connect(addr1).withdraw(depositAmt);
+      let receipt: ContractReceipt = await tx.wait();
 
-        const event = receipt.events?.find((x: any) => {
-          return x.event === "Withdrawal";
-        });
+      const event = receipt.events?.find((x: any) => {
+        return x.event === "Withdrawal";
+      });
 
-        expect(event?.args?.src).to.equal(await addr2.getAddress());
-        expect(event?.args?.wad).to.equal(depositAmt);
+      expect(event?.args?.src).to.equal(await addr1.getAddress());
+      expect(event?.args?.wad).to.equal(depositAmt);
     });
   });
 });
