a5a1df93dfa003e54cb954594360d5c0c3fd3082
drklee3 <derrick@dlee.dev>
2022-03-07 14:19:25 -0800

    Move contract query methods to testutil

--- x/bridge/keeper/erc20_test.go
+++ x/bridge/keeper/erc20_test.go
@@ -1,29 +1,12 @@
 package keeper_test
 
 import (
-	"encoding/hex"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	ethtypes "github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/core/vm"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-
-	"github.com/tharsis/ethermint/crypto/ethsecp256k1"
-	"github.com/tharsis/ethermint/server/config"
-	etherminttests "github.com/tharsis/ethermint/tests"
-	evmtypes "github.com/tharsis/ethermint/x/evm/types"
 
 	"github.com/kava-labs/kava-bridge/contract"
 	"github.com/kava-labs/kava-bridge/x/bridge/testutil"
@@ -58,46 +41,12 @@ func (suite *ERC20TestSuite) TestDeployERC20() {
 	suite.deployERC20()
 }
 
-func (suite *ERC20TestSuite) queryContract(
-	contractAbi abi.ABI,
-	from common.Address,
-	fromKey *ethsecp256k1.PrivKey,
-	contract types.InternalEVMAddress,
-	method string,
-	args ...interface{},
-) ([]interface{}, error) {
-	// Pack query args
-	data, err := contractAbi.Pack(method, args...)
-	suite.Require().NoError(err)
-
-	// Send TX
-	res := suite.sendTx(contract, from, fromKey, data)
-
-	// Check for VM errors and unpack returned data
-	switch res.VmError {
-	case vm.ErrExecutionReverted.Error():
-		response, err := abi.UnpackRevert(res.Ret)
-		suite.Require().NoError(err)
-
-		return nil, errors.New(response)
-	case "": // No error, continue
-	default:
-		panic(fmt.Sprintf("unhandled vm error response: %v", res.VmError))
-	}
-
-	// Unpack response
-	unpackedRes, err := contractAbi.Unpack(method, res.Ret)
-	suite.Require().NoErrorf(err, "failed to unpack method %v response", method)
-
-	return unpackedRes, nil
-}
-
 func (suite *ERC20TestSuite) TestERC20Query() {
 	contractAddr := suite.deployERC20()
 
 	// Query ERC20.decimals()
 	addr := common.BytesToAddress(suite.Key1.PubKey().Address())
-	res, err := suite.queryContract(
+	res, err := suite.QueryContract(
 		contract.ERC20MintableBurnableContract.ABI,
 		addr,
 		suite.Key1,
@@ -120,7 +69,7 @@ func (suite *ERC20TestSuite) TestERC20Mint_Unauthorized() {
 	addr := common.BytesToAddress(suite.Key1.PubKey().Address())
 	receiver := common.BytesToAddress(suite.Key2.PubKey().Address())
 	amount := big.NewInt(10)
-	_, err := suite.queryContract(
+	_, err := suite.QueryContract(
 		contract.ERC20MintableBurnableContract.ABI,
 		addr,
 		suite.Key1,
@@ -147,7 +96,7 @@ func (suite *ERC20TestSuite) TestERC20Mint() {
 
 	// Query ERC20.balanceOf()
 	addr := common.BytesToAddress(suite.Key1.PubKey().Address())
-	res, err := suite.queryContract(
+	res, err := suite.QueryContract(
 		contract.ERC20MintableBurnableContract.ABI,
 		addr,
 		suite.Key1,
@@ -161,73 +110,4 @@ func (suite *ERC20TestSuite) TestERC20Mint() {
 	balance, ok := res[0].(*big.Int)
 	suite.Require().True(ok, "balanceOf should respond with *big.Int")
 	suite.Require().Equal(big.NewInt(1234), balance)
-}
-
-func (suite *ERC20TestSuite) sendTx(
-	contractAddr types.InternalEVMAddress,
-	from common.Address,
-	signerKey *ethsecp256k1.PrivKey,
-	transferData []byte,
-) *evmtypes.MsgEthereumTxResponse {
-	ctx := sdk.WrapSDKContext(suite.Ctx)
-	chainID := suite.App.EvmKeeper.ChainID()
-
-	args, err := json.Marshal(&evmtypes.TransactionArgs{
-		To:   &contractAddr.Address,
-		From: &from,
-		Data: (*hexutil.Bytes)(&transferData),
-	})
-	suite.Require().NoError(err)
-	res, err := suite.QueryClientEvm.EstimateGas(ctx, &evmtypes.EthCallRequest{
-		Args:   args,
-		GasCap: uint64(config.DefaultGasCap),
-	})
-	suite.Require().NoError(err)
-
-	nonce := suite.App.EvmKeeper.GetNonce(suite.Ctx, suite.Address)
-
-	baseFee := suite.App.FeeMarketKeeper.GetBaseFee(suite.Ctx)
-	suite.Require().NotNil(baseFee, "base fee is nil")
-
-	// Mint the max gas to the FeeCollector to ensure balance in case of refund
-	suite.MintFeeCollector(sdk.NewCoins(
-		sdk.NewCoin(
-			"ukava",
-			sdk.NewInt(baseFee.Int64()*int64(res.Gas)),
-		)))
-
-	ercTransferTx := evmtypes.NewTx(
-		chainID,
-		nonce,
-		&contractAddr.Address,
-		nil,       // amount
-		res.Gas*2, // gasLimit, TODO: runs out of gas with just res.Gas, ex: estimated was 21572 but used 24814
-		nil,       // gasPrice
-		suite.App.FeeMarketKeeper.GetBaseFee(suite.Ctx), // gasFeeCap
-		big.NewInt(1), // gasTipCap
-		transferData,
-		&ethtypes.AccessList{}, // accesses
-	)
-
-	ercTransferTx.From = hex.EncodeToString(signerKey.PubKey().Address())
-	err = ercTransferTx.Sign(ethtypes.LatestSignerForChainID(chainID), etherminttests.NewSigner(signerKey))
-	suite.Require().NoError(err)
-
-	rsp, err := suite.App.EvmKeeper.EthereumTx(ctx, ercTransferTx)
-	suite.Require().NoError(err)
-	// Do not check vm error here since we want to check for errors later
-
-	return rsp
-}
-
-func (suite *ERC20TestSuite) MintFeeCollector(coins sdk.Coins) {
-	err := suite.App.BankKeeper.MintCoins(suite.Ctx, minttypes.ModuleName, coins)
-	suite.Require().NoError(err)
-	err = suite.App.BankKeeper.SendCoinsFromModuleToModule(
-		suite.Ctx,
-		minttypes.ModuleName,
-		authtypes.FeeCollectorName,
-		coins,
-	)
-	suite.Require().NoError(err)
 }
