c8d0ef51311e637e1674f53e8f57fed2745b2ac7
drklee3 <derrick@dlee.dev>
2022-03-08 16:44:25 -0800

    Test direct creation of pair validation

--- x/bridge/types/erc20_test.go
+++ x/bridge/types/erc20_test.go
@@ -51,7 +51,7 @@ func TestNewERC20BridgePair(t *testing.T) {
 			types.NewInternalEVMAddress(common.HexToAddress("0x00")),
 			errArgs{
 				expectPass: false,
-				contains:   "internal address cannot be zero value",
+				contains:   "external address cannot be zero value",
 			},
 		},
 	}
@@ -72,5 +72,61 @@ func TestNewERC20BridgePair(t *testing.T) {
 			}
 		})
 	}
+}
 
+func TestNewERC20BridgePair_Direct(t *testing.T) {
+	type errArgs struct {
+		expectPass bool
+		contains   string
+	}
+	tests := []struct {
+		name    string
+		pair    types.ERC20BridgePair
+		errArgs errArgs
+	}{
+		{
+			"valid",
+			types.ERC20BridgePair{
+				ExternalERC20Address: common.HexToAddress("0x01").Bytes(),
+				InternalERC20Address: common.HexToAddress("0x02").Bytes(),
+			},
+			errArgs{
+				expectPass: true,
+			},
+		},
+		{
+			"invalid - invalid external length",
+			types.ERC20BridgePair{
+				ExternalERC20Address: []byte{1},
+				InternalERC20Address: []byte{2},
+			},
+			errArgs{
+				expectPass: false,
+				contains:   "external address length is 1 but expected 20",
+			},
+		},
+		{
+			"invalid - invalid internal length",
+			types.ERC20BridgePair{
+				ExternalERC20Address: common.HexToAddress("0x01").Bytes(),
+				InternalERC20Address: []byte{2},
+			},
+			errArgs{
+				expectPass: false,
+				contains:   "internal address length is 1 but expected 20",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.pair.Validate()
+			if tc.errArgs.expectPass {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.errArgs.contains)
+			}
+		})
+	}
 }
