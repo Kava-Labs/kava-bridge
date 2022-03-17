883828988739073028f3e129154b778d0983bd9d
drklee3 <derrick@dlee.dev>
2022-03-17 14:12:21 -0700

    Remove unused event types

--- x/bridge/types/events.go
+++ x/bridge/types/events.go
@@ -2,15 +2,8 @@ package types
 
 // Events for the module
 const (
-	EventTypeDeposit  = "bridge_deposit"
-	EventTypeWithdraw = "bridge_withdraw"
-
 	// ERC20MintableBurnable event names
 	ContractEventTypeWithdraw = "Withdraw"
 
-	AttributeValueCategory           = ModuleName
-	AttributeKeyEthereumERC20Address = "ethereum_erc20_address"
-	AttributeKeyRelayer              = "relayer"
-	AttributeKeyReceiver             = "receiver"
-	AttributeKeySequence             = "sequence"
+	AttributeValueCategory = ModuleName
 )
