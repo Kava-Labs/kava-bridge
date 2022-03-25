6873fe7b885865713dce11d1f3eb30a3f4f5fe11
Nick DeLuca <nickdeluca08@gmail.com>
2022-03-25 02:42:48 +0000

    update contrib script with correct addresses

--- contrib/devnet/init-new-chain.sh
+++ contrib/devnet/init-new-chain.sh
@@ -2,11 +2,11 @@
 set -e
 
 validatorMnemonic="equip town gesture square tomorrow volume nephew minute witness beef rich gadget actress egg sing secret pole winter alarm law today check violin uncover"
-# kava1ak4pa9z2aty94ze2cs06wsdnkg9hsvfkp40r02
-# 0xedaa1e944aeac85a8b2ac41fa741b3b20b783136
+# kava1ffv7nhd3z6sych2qpqkk03ec6hzkmufy0r2s4c
+# 0xeDAA1E944aeAC85a8b2aC41fA741B3b20B783136
 faucetMnemonic="crash sort dwarf disease change advice attract clump avoid mobile clump right junior axis book fresh mask tube front require until face effort vault"
-# kava123fxg0l602etulhhcdm0vt7l57qya5wjcrwhzz
-# 0x5452643ffa7ab2be7ef7c376f62fdfa7804ed1d2
+# kava1adkm6svtzjsxxvg7g6rshg6kj9qwej8gwqadqd
+# 0xeb6dBd418B14A063311e46870BA3569140ECC8e8
 
 
 DATA=~/.kava-bridged
@@ -52,7 +52,7 @@ sed -in-place='' 's/aphoton/ukava/g' $DATA/config/genesis.json
 jq '.app_state.bank.supply = []' $DATA/config/genesis.json | sponge $DATA/config/genesis.json
 
 # Set relayer to facuet address
-jq '.app_state.bridge.params.relayer = "kava123fxg0l602etulhhcdm0vt7l57qya5wjcrwhzz"' $DATA/config/genesis.json | sponge $DATA/config/genesis.json
+jq '.app_state.bridge.params.relayer = "kava1adkm6svtzjsxxvg7g6rshg6kj9qwej8gwqadqd"' $DATA/config/genesis.json | sponge $DATA/config/genesis.json
 
 # Set enabled erc20 tokens to match ropsten testnet
-jq '.app_state.bridge.params.enabled_erc20_tokens = [{address: "0xc778417e063141139fce010982780140aa0cd5ab", name: "Wrapped ETH", symbol: "WETH", decimals: 18},{address: "0x07865c6e87b9f70255377e024ace6630c1eaa37f", name: "USDC", symbol: "USDC", decimals: 18}]' $DATA/config/genesis.json | sponge $DATA/config/genesis.json
+jq '.app_state.bridge.params.enabled_erc20_tokens = [{address: "0xc778417e063141139fce010982780140aa0cd5ab", name: "Wrapped ETH", symbol: "WETH", decimals: 18},{address: "0x07865c6e87b9f70255377e024ace6630c1eaa37f", name: "USDC", symbol: "USDC", decimals: 6}]' $DATA/config/genesis.json | sponge $DATA/config/genesis.json
