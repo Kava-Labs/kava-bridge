cd0d7e296b2326df788bfa2932d4293e3eb0290c
Nick DeLuca <nickdeluca08@gmail.com>
2022-03-31 21:30:22 +0000

    connect to eth on 8555 locally; fixes start-geth script for changed port

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -33,7 +33,7 @@ const config: HardhatUserConfig = {
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     localhost: {
-      url: "http://127.0.0.1:8545",
+      url: "http://127.0.0.1:8555",
       accounts: "remote",
     },
   },
