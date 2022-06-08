21acc7bd6bcf2a6a3b6f2ee3ae8e9d0644785ddb
drklee3 <derrick@dlee.dev>
2022-06-07 17:53:14 -0700

    Save libp2p priv keys to reuse in test and keygen

--- relayer/testutil/keys.go
+++ relayer/testutil/keys.go
@@ -2,10 +2,49 @@ package testutil
 
 import (
 	"crypto/rand"
+	"fmt"
+	"path"
 
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/tendermint/tendermint/libs/os"
 )
+
+func p2pNodeTestKeyPath(index int) string {
+	return path.Join("..", "testutil", "test-fixtures", fmt.Sprintf("libp2p-%02d.key", index))
+}
+
+func ReadP2pNodeTestKey(index int) crypto.PrivKey {
+	path := p2pNodeTestKeyPath(index)
+
+	bytes := os.MustReadFile(path)
+	key, err := crypto.UnmarshalPrivateKey(bytes)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return key
+}
+
+func GetTestP2pNodeKeys(count int) []crypto.PrivKey {
+	var keys []crypto.PrivKey
+	for i := 0; i < count; i++ {
+		key := ReadP2pNodeTestKey(i)
+		keys = append(keys, key)
+	}
+
+	return keys
+}
+
+func WriteP2pNodeTestKey(index int, key crypto.PrivKey) {
+	bz, err := crypto.MarshalPrivateKey(key)
+	if err != nil {
+		panic(err)
+	}
+
+	os.MustWriteFile(p2pNodeTestKeyPath(index), bz, 0600)
+}
 
 // GenerateNodeKeys returns a slice of private keys.
 func GenerateNodeKeys(n int) ([]crypto.PrivKey, error) {
