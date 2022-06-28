ca9545122893dcb92ad7bce24308d7adff64d468
drklee3 <derrick@dlee.dev>
2022-06-28 14:46:41 -0700

    Add SetSessionID tests

--- relayer/session/signing_store_test.go
+++ relayer/session/signing_store_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/binance-chain/tss-lib/ecdsa/keygen"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/kava-labs/kava-bridge/relayer/broadcast"
+	"github.com/kava-labs/kava-bridge/relayer/mp_tss/types"
 	"github.com/kava-labs/kava-bridge/relayer/session"
 	"github.com/kava-labs/kava-bridge/relayer/testutil"
 	"github.com/stretchr/testify/require"
@@ -38,4 +39,44 @@ func TestSigningSessionStore(t *testing.T) {
 	require.True(t, found, "should find session that exists")
 
 	require.Equal(t, sess, sess2, "should return same session")
+}
+
+func TestSetSessionID(t *testing.T) {
+	store := session.NewSigningSessionStore()
+
+	txHash := common.Hash{1}
+	sessID := types.AggregateSigningSessionID{1, 2, 3}
+
+	sess, _, err := store.NewSession(
+		context.Background(),
+		&broadcast.NoOpBroadcaster{},
+		txHash,
+		nil,
+		0,
+		"",
+		testutil.TestPeerIDs,
+		nil,
+		nil,
+		keygen.LocalPartySaveData{},
+	)
+
+	require.NoError(t, err)
+
+	store.SetSessionID(txHash, sessID)
+
+	sess2, found := store.GetSessionFromID(sessID)
+	require.True(t, found, "should find session that exists")
+
+	require.Equal(t, sess, sess2)
+}
+
+func TestSetSessionID_NotFound(t *testing.T) {
+	store := session.NewSigningSessionStore()
+
+	sessID := types.AggregateSigningSessionID{1, 2, 3}
+
+	sess, found := store.GetSessionFromID(sessID)
+	require.False(t, found, "should find session that does not exist")
+
+	require.Nil(t, sess)
 }
